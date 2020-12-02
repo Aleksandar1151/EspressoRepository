8750e504a398308b2c0003cdbb747d93ea556974
Aleksandar <jovanovic.a.996@gmail.com>
2020-12-02 17:20:56 +0100

    Povezana MySQL baza

---
+++ EspressoProject/Classes/User.cs
@@ -0,0 +1,44 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EspressoProject.Classes
+{
+    class User
+    {
+
+        public static List<string> GetNames()
+        {
+            List<string> resultList = new List<string>();
+
+            try
+            {
+
+                String query = "SELECT * FROM nalozi;";
+                MySqlCommand cmd = new MySqlCommand(query, MainWindow.dbConn);
+                MainWindow.dbConn.Open();
+
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+                    String result = reader["korisnicko_ime"].ToString();
+                    resultList.Add(result);
+                }
+
+                MainWindow.dbConn.Close();
+
+
+            }
+            catch (Exception ex) { MessageBox.Show("Greška prilikom preuzimanja robe.\nRazlog: " + ex.Message); }
+
+            return resultList;
+        }
+    }
+}
--- EspressoProject/MainWindow.xaml.cs
+++ EspressoProject/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using EspressoProject.Classes;
 
 namespace EspressoProject
 {
@@ -22,19 +25,42 @@ namespace EspressoProject
     public partial class MainWindow : Window
     {
         public static LoginUC LoginPage = new LoginUC();
+
+        public static MySqlConnection dbConn;
         public MainWindow()
         {
+            try { InitializeDB(); }
+            catch (Exception ex) { MessageBox.Show("Greška prilikom pokretanja baze podataka\nGreška:" + ex.Message); this.Close(); }
+
             InitializeComponent();
 
-           // GridMain.Children.Clear();
+            // GridMain.Children.Clear();
             //GridMain.Children.Add(LoginPage);
 
             //Test1
+
+            List<string> names = User.GetNames();
+            
+            foreach(String name in names)
+            {
+                Console.WriteLine(name);
+            }
+           
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             
+        }
+
+
+       
+        public static void InitializeDB()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["myDatabaseConnection"].ConnectionString;
+            dbConn = new MySqlConnection(connectionString);
+
         }
     }
 }
